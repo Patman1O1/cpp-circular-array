3e684e3dda6b4169c0f4fa879ee697b043689ac0
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-22 16:49:56 -0500

    Remove compile_time_test.sh

--- scripts/compile_time_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
-
-conan build "${ROOT}" --target circular_array_compile_time_test -o "&:build_tests=True"
-"${ROOT}/build/Release/bin/circular_array_compile_time_test"
-
