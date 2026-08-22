c95189c255c4cbff3ffecbdae426ac9ab300b23d
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-22 13:29:58 -0500

    Fix path

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,6 @@
 
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
-conan build . "${ROOT}" -o "&:build_tests=True"
+conan build "${ROOT}" -o "&:build_tests=True"
 "${ROOT}/build/Release/bin/circular_array_test"
 
