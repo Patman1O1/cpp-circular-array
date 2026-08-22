63fc9e03927a0c0d32d625208b29722bd26980b5
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-22 13:30:05 -0500

    Fix path

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,6 +2,6 @@
 
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
-conan build . "${ROOT}" -o "&:build_tests=True" -s build_type=Debug
+conan build "${ROOT}" -o "&:build_tests=True" -s build_type=Debug
 gdb "${ROOT}/build/Debug/bin/circular_array_test"
 
